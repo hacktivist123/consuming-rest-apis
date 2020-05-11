6c064492ab3a2d9114bea91084defb907a94fd33
hacktivist123 <akintayoshedrack@gmail.com>
2020-05-11 18:52:41 +0100

    finished demo app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import List from './components/List';
+import withListLoading from './components/withListLoading';
 
 function App() {
+  const ListLoading = withListLoading(List);
+  const [AppState, SetAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    SetAppState({ loading: true });
+    fetch(`https://api.github.com/users/hacktivist123/repos`)
+      .then((res) => res.json())
+      .then((repos) => {
+        SetAppState({ loading: false, repos: repos });
+      });
+  }, [SetAppState]);
   return (
     <div className='App'>
       <div className='container'>
         <h1>My Repositories</h1>
       </div>
-      <div className='repo-container'></div>
+      <div className='repo-container'>
+        <ListLoading isLoading={AppState.loading} repos={AppState.repos} />
+      </div>
+      <footer>
+        <div className='footer'>
+          Built{' '}
+          <span role='img' aria-label='emoji'>
+            💚
+          </span>{' '}
+          with by Shedrack Akintayo
+        </div>
+      </footer>
     </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,20 @@
+import React from 'react';
+const List = (props) => {
+  const { repos } = props;
+  if (!repos) return null;
+  if (!repos.length) return <p>No repos, sorry</p>;
+  return (
+    <ul>
+         <h2 className='list-head'>Available Public Repositories</h2>
+      {repos.map((repo) => {
+        return (
+          <li key={repo.id} className='list'>
+            <span className='repo-text'>{repo.full_name} </span>
+            <span className='repo-description'>{repo.description}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+export default List;
