7b28fe9f7a9179a7dfd0ece4cf56df18ca550f20
hacktivist123 <akintayoshedrack@gmail.com>
2020-05-18 13:56:58 +0100

    write example for using fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class myComponent extends React.Component {
+  componentDidMount() {
+    const apiUrl = 'https://api.github.com/users/hacktivist123/repos';
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => console.log('This is your data', data));
+  }
+  render() {
+    return <h1>my Component has Mounted</h1>;
+  }
 }
 
-export default App;
+export default myComponent;
