92fe33d02739e6caaac137b560008c03d5744efe
hacktivist123 <akintayoshedrack@gmail.com>
2020-05-18 16:30:06 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React from 'react';
-class myComponent extends React.Component {
-  componentDidMount() {
-    const apiUrl = 'https://api.github.com/users/hacktivist123/repos';
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => console.log('This is your data', data));
-  }
-  render() {
-    return <h1>my Component has Mounted</h1>;
-  }
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import List from './components/List';
+import withListLoading from './components/withListLoading';
+
+function App() {
+  const ListLoading = withListLoading(List);
+  const [AppState, SetAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    SetAppState({ loading: true });
+    const user = `https://api.github.com/users/hacktivist123/repos`;
+    axios.get(user).then((repos) => {
+      const allRepos = repos.data;
+      SetAppState({ loading: false, repos: allRepos });
+    });
+  }, [SetAppState]);
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>My Repositories</h1>
+      </div>
+      <div className='repo-container'>
+        <ListLoading isLoading={AppState.loading} repos={AppState.repos} />
+      </div>
+      <footer>
+        <div className='footer'>
+          Built{' '}
+          <span role='img' aria-label='emoji'>
+            💚
+          </span>{' '}
+          with by Shedrack Akintayo
+        </div>
+      </footer>
+    </div>
+  );
 }
 
-export default myComponent;
+export default App;
---
+++ src/components/withListLoading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function WithListLoading(Component) {
+  return function WihLoadingComponent({ isLoading, ...props }) {
+    if (!isLoading) return <Component {...props} />;
+    return (
+      <p style={{ textAlign: 'center', fontSize: '30px' }}>
+        Hold on, fetching data may take some time :)
+      </p>
+    );
+  };
+}
+export default WithListLoading;
